96aaea033d2d17972c321b7d692c27982feda9b5
dttncl <82695034+dttncl@users.noreply.github.com>
2024-02-15 15:36:20 -0500

    updated dashboard list views

---
+++ app/src/main/java/com/example/bill_management_app/CustomTransactionsAdapter.java
@@ -0,0 +1,56 @@
+package com.example.bill_management_app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.Date;
+
+public class CustomTransactionsAdapter extends BaseAdapter {
+    Context context;
+    String transactionId[];
+    String billerId[];
+    String transactionDate[];
+
+    LayoutInflater inflater;
+
+    public CustomTransactionsAdapter(Context appContext, String[] transactionId, String[] billerId, String[] transactionDate) {
+        context = appContext;
+        this.transactionId = transactionId;
+        this.billerId = billerId;
+        this.transactionDate = transactionDate;
+
+        inflater = LayoutInflater.from(appContext);
+    }
+    @Override
+    public int getCount() {
+        return transactionId.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        convertView = inflater.inflate(R.layout.list_mngr_transactions,null);
+        TextView tId = convertView.findViewById(R.id.transactionItem);
+        TextView bId = convertView.findViewById(R.id.billerItem);
+        TextView tDate = convertView.findViewById(R.id.dateItem);
+
+        tId.setText(transactionId[position]);
+        bId.setText(billerId[position]);
+        tDate.setText(transactionDate[position]);
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/bill_management_app/ManagerDashboard.java
+++ app/src/main/java/com/example/bill_management_app/ManagerDashboard.java
@@ -20,17 +20,23 @@ public class ManagerDashboard extends AppCompatActivity {
         listViewCustomers = findViewById(R.id.listCustomers);
         listViewTransactions = findViewById(R.id.listTransactions);
 
-
         String[] customers = {"BBC0001","BBC0002","BBC0003","BBC0004","BBC0005"};
         String[] transactions = {"BBT0000101","BBT0000102","BBT0000103","BBT0000104","BBT0000105"};
+        String[] billers = {"BBB500","BBB501","BBB502","BBB501","BBB503"};
+        String[] tDates = {"02/12/2024","02/07/2024","01/28/2024","12/30/2023","12/28/2023"};
 
-        LayoutInflater inflater = getLayoutInflater();
-        ViewGroup header = (ViewGroup)inflater.inflate(R.layout.list_mngr_customer_header,listViewCustomers,false);
-        listViewCustomers.addHeaderView(header,null,false);
+        // set header for customers list
+        LayoutInflater inflaterCustomer = getLayoutInflater();
+        ViewGroup headerCustomer = (ViewGroup)inflaterCustomer.inflate(R.layout.list_mngr_customer_header,listViewCustomers,false);
+        listViewCustomers.addHeaderView(headerCustomer,null,false);
+
+        // set header for transactions list
+        LayoutInflater inflaterTransaction = getLayoutInflater();
+        ViewGroup headerTransaction = (ViewGroup)inflaterTransaction.inflate(R.layout.list_mngr_transactions_header,listViewCustomers,false);
+        listViewTransactions.addHeaderView(headerTransaction,null,false);
 
         ArrayAdapter<String> adapterCustomers = new ArrayAdapter<>(this,R.layout.list_mngr_customer,R.id.customerItem,customers);
-        ArrayAdapter<String> adapterTransactions = new ArrayAdapter<>(this,R.layout.list_mngr_customer,R.id.customerItem,transactions);
-
+        CustomTransactionsAdapter adapterTransactions = new CustomTransactionsAdapter(getApplicationContext(),transactions,billers,tDates);
 
         listViewCustomers.setAdapter(adapterCustomers);
         listViewTransactions.setAdapter(adapterTransactions);
