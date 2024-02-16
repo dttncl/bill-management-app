67553b154ba7de84709fcb65c24f51ecadec430e
dttncl <82695034+dttncl@users.noreply.github.com>
2024-02-16 14:26:04 -0500

    added detailed transaction history view

---
+++ app/src/main/java/com/example/bill_management_app/ManagerCustomerTransactionView.java
@@ -0,0 +1,14 @@
+package com.example.bill_management_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ManagerCustomerTransactionView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_manager_customer_transaction_view);
+    }
+}
