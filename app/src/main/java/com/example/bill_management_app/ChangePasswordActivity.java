9b307c76304052d757ad96de006cb130ceb26d67
dttncl <82695034+dttncl@users.noreply.github.com>
2024-03-15 12:02:33 -0400

    feat: added change password

---
+++ app/src/main/java/com/example/bill_management_app/ChangePasswordActivity.java
@@ -0,0 +1,113 @@
+package com.example.bill_management_app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    LinearLayout navIcons;
+    ImageButton btnHome;
+    Button btnLogout, buttonUpdatePassword, buttonUpdateCancel;
+    EditText txtCurrentPassword, txtNewPassword, txtConfirmNewPassword;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_change_password);
+
+        txtCurrentPassword = findViewById(R.id.txtCurrentPassword);
+        txtNewPassword = findViewById(R.id.txtNewPassword);
+        txtConfirmNewPassword = findViewById(R.id.txtConfirmNewPassword);
+        buttonUpdatePassword = findViewById(R.id.buttonUpdatePassword);
+        buttonUpdateCancel = findViewById(R.id.buttonUpdateCancel);
+
+        // extract the intent extras
+        Intent intent = getIntent();
+        Client oneClient = (Client) intent.getSerializableExtra("oneClient");
+
+        String oldPassword = oneClient.getPassword();
+
+        buttonUpdateCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangePasswordActivity.this, ClientProfilePageActivity.class);
+                intent.putExtra("oneClient", oneClient);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonUpdatePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String currentPassword = txtCurrentPassword.getText().toString().trim();
+                if (oldPassword.equals(currentPassword)) {
+                    String newPassword = txtNewPassword.getText().toString().trim();
+                    String newConfirmPassword = txtConfirmNewPassword.getText().toString().trim();
+
+                    if(!Validator.isValidPassword(newPassword)) {
+                        txtNewPassword.setError("Password must contain the following: \n - Between 8 and 12 characters in length.\n" +
+                                "Contain at least one digit.\n" +
+                                "Contain at least one special character [!@#$%^&*()_+\\-=[]{};':\"|,.<>/?].\n" +
+                                "Contain at least one alphabet character.");
+                        txtNewPassword.setText("");
+                        txtNewPassword.requestFocus();
+                        return;
+                    }
+
+                    if(!newConfirmPassword.equals(newPassword)) {
+                        txtConfirmNewPassword.setError("Password and Confirm Password does not match");
+                        txtConfirmNewPassword.setText("");
+                        return;
+                    }
+
+                    oneClient.setPassword(newPassword);
+                    Intent intent = new Intent(ChangePasswordActivity.this, ClientProfilePageActivity.class);
+                    intent.putExtra("oneClient", oneClient);
+                    startActivity(intent);
+                    finish();
+
+                } else {
+                    txtCurrentPassword.setError("Incorrect Password");
+                    txtCurrentPassword.setText("");
+                    return;
+                }
+            }
+        });
+
+        // HEADER ICONS FUNCTIONALITY
+        navIcons = findViewById(R.id.includeTopIcons);
+        btnLogout = navIcons.findViewById(R.id.btnLogout);
+        btnHome = navIcons.findViewById(R.id.btnHome);
+
+        btnLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Intent intent = new Intent(ChangePasswordActivity.this, LoginPageActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ChangePasswordActivity.this, ClientDashboard.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/bill_management_app/ClientProfilePageActivity.java
+++ app/src/main/java/com/example/bill_management_app/ClientProfilePageActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,16 +16,18 @@ import com.google.firebase.auth.FirebaseAuth;
 
 public class ClientProfilePageActivity extends AppCompatActivity {
 
-    TextView textViewFirstName;
-    TextView textViewLastName;
-    TextView textViewPhone;
-    TextView textViewEmail;
+    EditText textViewFirstName;
+    EditText textViewLastName;
+    EditText textViewPhone;
+    EditText textViewEmail;
 
     TextView textViewCredit;
 
     LinearLayout navIcons;
     ImageButton btnHome;
-    Button btnLogout;
+    Button btnLogout, btnChangePassword, btnSaveProfile;
+
+    boolean isClicked = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,23 +39,15 @@ public class ClientProfilePageActivity extends AppCompatActivity {
         textViewPhone = findViewById(R.id.textViewRealPhoneProfilePage);
         textViewEmail = findViewById(R.id.textViewRealEmailProfilePage);
         textViewCredit = findViewById(R.id.textViewBalanceProfilePage);
-        Intent intent = getIntent();
+
+        btnChangePassword = findViewById(R.id.buttonChangePassword);
+        btnSaveProfile = findViewById(R.id.buttonSaveProfile);
 
         // extract the intent extras
+        Intent intent = getIntent();
         Client oneClient = (Client) intent.getSerializableExtra("oneClient");
-        String firstName = oneClient.getFirstName();
-        String lastName = oneClient.getLastName();
-        String phone = oneClient.getPhone();
-        String email = oneClient.getEmail();
-        double credit = oneClient.getCredit();
-
-        // display
-        textViewFirstName.setText(firstName);
-        textViewLastName.setText(lastName);
-        textViewPhone.setText(phone);
-        textViewEmail.setText(email);
-        textViewCredit.setText(String.valueOf(credit));
-
+        Toast.makeText(this, oneClient.getPassword(), Toast.LENGTH_SHORT).show();
+        DisplayProfile(oneClient);
 
         // HEADER ICONS FUNCTIONALITY
         navIcons = findViewById(R.id.includeTopIcons);
@@ -74,10 +69,86 @@ public class ClientProfilePageActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent = new Intent(ClientProfilePageActivity.this, ClientDashboard.class);
                 startActivity(intent);
+                finish();
             }
         });
 
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
+
+        btnSaveProfile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String nfName = textViewFirstName.getText().toString().trim();
+                String nlName = textViewLastName.getText().toString().trim();
+                String nPhone = textViewPhone.getText().toString().trim();
+                String nEmail = textViewEmail.getText().toString().trim();
+                String nCredit = textViewCredit.getText().toString().trim();
+            }
+        });
+
+        btnChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ClientProfilePageActivity.this, ChangePasswordActivity.class);
+                intent.putExtra("oneClient", oneClient);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+
+    private void DisplayProfile (Client oneClient){
+        String firstName = oneClient.getFirstName();
+        String lastName = oneClient.getLastName();
+        String phone = oneClient.getPhone();
+        String email = oneClient.getEmail();
+        double credit = oneClient.getCredit();
+
+        textViewFirstName.setText(firstName);
+        textViewLastName.setText(lastName);
+        textViewPhone.setText(phone);
+        textViewEmail.setText(email);
+        textViewCredit.setText(String.valueOf(credit));
+    }
+
+    private void ToggleEdit (View view){
+
+        if (isClicked) {
+            view.setFocusable(true);
+            view.setFocusableInTouchMode(true);
+            view.setClickable(true);
+            view.setLongClickable(true);
+            isClicked = false;
+        } else {
+            view.setFocusable(false);
+            view.setFocusableInTouchMode(false);
+            view.setClickable(false);
+            view.setLongClickable(false);
+            isClicked = true;
+        }
+    }
+
+    public void onEditMode(View view) {
+        int viewId = view.getId();
+        if (viewId == R.id.buttonFirstNameProfilePage) {
+            ToggleEdit(textViewFirstName);
+            textViewFirstName.setText(textViewFirstName.getText().toString());
+        } else if (viewId == R.id.buttonLastName) {
+            ToggleEdit(textViewLastName);
+            textViewLastName.setText(textViewLastName.getText().toString());
+        } else if (viewId == R.id.buttonPhone) {
+            ToggleEdit(textViewPhone);
+            textViewPhone.setText(textViewPhone.getText().toString());
+        } else if (viewId == R.id.buttonEmail) {
+            ToggleEdit(textViewEmail);
+            textViewEmail.setText(textViewEmail.getText().toString());
+        }
     }
 
 }
